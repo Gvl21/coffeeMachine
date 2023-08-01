f3230d860d0471295e080c101da92b2d2420d12d
Gvl21 <thornyk1@gmail.com>
2023-08-01 11:35:57 +0900

    대략적인 메소드 실행 구현

--- ReadMe.md
+++ ReadMe.md
@@ -2,7 +2,7 @@
 - Java Programming으로 Coffee Machine 만들어 보기
 
 ### 실행방법
-- src/CoffeMachine.java의 메인메소드 실행
+- src/CoffeeMachine.java의 메인메소드 실행
 
 ### 어떤 기술이 사용되었는지
 
---
+++ src/Ingredient.java
@@ -0,0 +1,8 @@
+public class Ingredient {
+    int money = 50000;
+    int water = 400;
+    int milk = 540;
+    int coffeeBeans = 120;
+    int disposableCup = 9;
+
+}
